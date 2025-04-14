663e4d2ba457490b2b192e1bb57e1c933b194189
mobtaker61 <m.totonchi@hotmail.com>
2025-04-14 23:00:21 +0400

    srv-up7

--- database/migrations/2025_04_13_183514_create_settings_table.php
+++ database/migrations/2025_04_13_183514_create_settings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('key', 191)->unique();
             $table->text('value')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
